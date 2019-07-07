240df34fdfd64c2b2d560df56cb9854295165537
kuroteren <kuroteren@gmail.com>
2019-07-07 08:08:56 -0700

    Update Options Loss Recovery Calculator.cpp
    Adjusted for loop limits
    Added output math display for profit

--- Options Loss Recovery Calculator.cpp
+++ Options Loss Recovery Calculator.cpp
@@ -9,14 +9,16 @@ by: Alexander Hess
 
 using namespace std;
 
-list<pair<int, float>> calculateRecovery(float cost, float loss, float price);
+list<pair<int, float>> calculateRecovery(float cost, float loss, float price, int maxOptions);
 
 int main()
 {
 	//variables
-	float callCost, loss, callPrice;
-	int shares, options;
+	float callCost, loss, callPrice, totalExpense;
+	int shares, options, maxOptions;
 	list<pair<int, float>> minCounts;
+	bool showMath;
+	char temp;
 
 	//Intro output
 	cout << "Greetings\n\tThis calculator is meant to provide minimum sale price of excersized call options\n\tin order to recover a pre-existing loss" << endl << endl;
@@ -33,23 +35,61 @@ int main()
 	cout << "Current total loss you aim to cover: ";
 	cin >> loss;
 
+	cout << "Enter the max number of options you are willing to buy: ";
+	cin >> maxOptions;
+
+	cout << "Would you like the profit math shown at the end? (Y/N): ";
+	cin >> temp;
+
+	if (toupper(temp) == 'Y')
+	{
+		showMath = true;
+	}
+	else
+	{
+		showMath = false;
+	}
+
 	//Spacer
 	cout << endl << endl << endl;
 
-	//Calculate Options count and price values
-	minCounts = calculateRecovery(callCost, loss, callPrice);
+	totalExpense = callCost + callPrice;
 
-	//Iterate for output
-	for (auto& itr : minCounts)
+	//Calculate Options count and price values
+	minCounts = calculateRecovery(callCost, loss, callPrice, maxOptions);
+
+	//On the off chance that no results are returned
+	if (minCounts.size() == 0)
 	{
-		cout << "For " << itr.first << " options, minimum sale price to recover profits is: $";
-		printf("%.2f", itr.second);
-		cout << endl;
+		cout << "Unfortunately no amount of options can reasonably cover your losses." << endl;
+	}
+	else
+	{
+		//Iterate for output
+		for (auto& itr : minCounts)
+		{
+			cout << "For " << itr.first << " options, minimum sale price to recover losses is: $";
+			printf("%.2f", itr.second);
+			cout << "\n\t This gives a total profit gain of $";
+			printf("%.2f", ((itr.first * 100.00 * itr.second) - ((itr.first * 100.00 * totalExpense) + loss)));
+
+			if (showMath)
+			{
+				cout << "\n\t Options sale value: ";
+				cout << itr.first << " options * 100 shares * $" << itr.second << " = $";
+				printf("%.2f", (itr.first * 100.00 * itr.second));
+				cout << "\n\t Total loss and option expenses: ";
+				cout << itr.first << " options * 100 shares * " << totalExpense << " + " << loss << "= $";
+				printf("%.2f", ((itr.first * 100.00 * totalExpense) + loss));
+			}
+
+			cout << endl << endl;
+		}
 	}
 }
 
 
-list<pair<int, float>> calculateRecovery(float cost, float loss, float price)
+list<pair<int, float>> calculateRecovery(float cost, float loss, float price, int maxOptions)
 {
 	list<pair<int, float>> values{};
 	//Ease of use total cost variable
@@ -60,18 +100,16 @@ list<pair<int, float>> calculateRecovery(float cost, float loss, float price)
 	loop set to break at 25 options, arbitrarily
 		*will change in future to allow user set limit
 	*/
-	for (int i = 1; i > 25; i++)
+	for (int i = 1; i <= maxOptions; i++)
 	{
 		//Minimum price loop
 		/*
 		j = costPriceTotal plus one cent as starting point
 		this is due to any price less will not cover cost of options
 
-		break value is costPriceTotal times 10, arbitrarily
-		Likely unreachable unless loss value is substantial
-			*marked for refactoring
+		break value is costPriceTotal plus loss divided by 80
 		*/
-		for (float j = costPriceTotal + 0.01; j > (costPriceTotal * 10); j += 0.01)
+		for (float j = costPriceTotal + 0.01; j <= ((costPriceTotal + loss) / 80); j += 0.01)
 		{
 			/* 
 			If Price is above a net-zero sum, add it
@@ -101,8 +139,10 @@ list<pair<int, float>> calculateRecovery(float cost, float loss, float price)
 
 /********Running Task List**********/
 /*
-->Allow user to set max options value
-->Redo sale value max to accomodate large loss possibilities
 ->Multi-thread!!!!
 ->Expand calculator to take stock purchases outside of Options
+->Add specific option count calculator, get minimum sell price for a specific number of options
 */
+
+//So many comments!!!!
+//Seriously think I commented this more than wrote code. -.-
