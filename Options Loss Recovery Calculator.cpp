f12827632d16b2e6a3cda12d7018e5bdd4f11e76
kuroteren <kuroteren@gmail.com>
2019-07-09 07:53:06 -0700

    Multi-threaded
    Added multithreading to program.
    adjusted calculator function to work in multi-threaded environment.

--- Options Loss Recovery Calculator.cpp
+++ Options Loss Recovery Calculator.cpp
@@ -6,19 +6,27 @@ by: Alexander Hess
 
 #include <iostream>
 #include <list>
+#include <thread>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-list<pair<int, float>> calculateRecovery(float cost, float loss, float price, int maxOptions);
+static void calculateRecovery(float cost, float loss, float price, int options, list<pair<int, float>> *minCounts);
+
+void join_thread(thread& t);
+
+void join_all_threads(vector<thread>& v);
 
 int main()
 {
 	//variables
 	float callCost, loss, callPrice, totalExpense;
-	int shares, options, maxOptions;
-	list<pair<int, float>> minCounts;
+	int maxOptions;
+	static list<pair<int, float>> * minCounts = new list<pair<int,float>>();
 	bool showMath;
 	char temp;
+	vector<thread> threadVector;
 
 	//Intro output
 	cout << "Greetings\n\tThis calculator is meant to provide minimum sale price of excersized call options\n\tin order to recover a pre-existing loss" << endl << endl;
@@ -56,17 +64,24 @@ int main()
 	totalExpense = callCost + callPrice;
 
 	//Calculate Options count and price values
-	minCounts = calculateRecovery(callCost, loss, callPrice, maxOptions);
+	for (int i = 1; i <= maxOptions; i++)
+	{
+		threadVector.push_back(thread( calculateRecovery, callCost, loss, callPrice, i, ref(minCounts) ));
+	}
+
+	join_all_threads(threadVector);
+
+	(*minCounts).sort();
 
 	//On the off chance that no results are returned
-	if (minCounts.size() == 0)
+	if ((*minCounts).size() == 0)
 	{
 		cout << "Unfortunately no amount of options can reasonably cover your losses." << endl;
 	}
 	else
 	{
 		//Iterate for output
-		for (auto& itr : minCounts)
+		for (auto& itr : (*minCounts))
 		{
 			cout << "For " << itr.first << " options, minimum sale price to recover losses is: $";
 			printf("%.2f", itr.second);
@@ -88,58 +103,55 @@ int main()
 	}
 }
 
-
-list<pair<int, float>> calculateRecovery(float cost, float loss, float price, int maxOptions)
+static void calculateRecovery(float cost, float loss, float price, int options, list<pair<int, float>>* minCounts)
 {
-	list<pair<int, float>> values{};
 	//Ease of use total cost variable
 	float costPriceTotal = cost + price;
 
-	//Options # loop
+	//Minimum price loop
 	/*
-	loop set to break at 25 options, arbitrarily
-		*will change in future to allow user set limit
+	j = costPriceTotal plus one cent as starting point
+	this is due to any price less will not cover cost of options
+
+	break value is costPriceTotal plus loss divided by 80
 	*/
-	for (int i = 1; i <= maxOptions; i++)
+	for (float j = costPriceTotal + 0.01; j <= (costPriceTotal + loss); j += 0.01)
 	{
-		//Minimum price loop
 		/*
-		j = costPriceTotal plus one cent as starting point
-		this is due to any price less will not cover cost of options
+		If Price is above a net-zero sum, add it
+		then break loop to move to next options value
 
-		break value is costPriceTotal plus loss divided by 80
+		(options * 100 * j) -> options count times 100 shares per option times sell value = sale value
+		subtract
+		(costPriceTotal * 100 * options) -> total cost per share of option times 100 shares per option times total number of options
+		then subtract other losses desired to be covered
+
+		if total is greater than 0, value j is minimum needed to cover costs and losses
 		*/
-		for (float j = costPriceTotal + 0.01; j <= ((costPriceTotal + loss) / 80); j += 0.01)
+		if (((options * 100 * j) - (costPriceTotal * 100 * options) - loss) >= 0)
 		{
-			/* 
-			If Price is above a net-zero sum, add it
-			then break loop to move to next options value
-			 
-			(i * 100 * j) -> options count times 100 shares per option times sell value = sale value
-			subtract
-			(costPriceTotal * 100 * i) -> total cost per share of option times 100 shares per option times total number of options
-			then subtract other losses desired to be covered
+			//Add Options value and price value (i,j) into pair
+			pair<int, float> temp = make_pair(options, j);
 
-			if total is greater than 0, value j is minimum needed to cover costs and losses
-			*/
-			if (((i * 100 * j) - (costPriceTotal * 100 * i) - loss) > 0)
-			{
-				//Add Options value and price value (i,j) into pair
-				pair<int,float> temp = make_pair(i,j);
-
-				//push to list
-				values.push_back(temp);
-				break;
-			}
+			//push to list
+			(*minCounts).push_back(temp);
+			break;
 		}
 	}
+}
 
-	return values;
+void join_thread(thread& t)
+{
+	t.join();
+}
+
+void join_all_threads(vector<thread>& v)
+{
+	for_each(v.begin(), v.end(), join_thread);
 }
 
 /********Running Task List**********/
 /*
-->Multi-thread!!!!
 ->Expand calculator to take stock purchases outside of Options
 ->Add specific option count calculator, get minimum sell price for a specific number of options
 */
